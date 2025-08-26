1b727c388f359eaabfe7338ef13e9749851e1efb
nitesh6217 <nitesh6217maths@rla.du.ac.in>
2025-08-26 16:53:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
+      hum = body.querySelector(".hum")
       logo = body.querySelector(".logo")
 
+      hum.addEventListener("click",() =>{
+        sidebar.classList.toggle("close");
+        
+      });
       logo.addEventListener("click",() =>{
         sidebar.classList.toggle("close");
+        
       });
 
       
